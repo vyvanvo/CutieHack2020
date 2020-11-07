5ea1208cc78cd3c9caf69ff1b19feaee1a9f18b4
bran-dont <hellobrandontran@gmail.com>
2020-11-07 15:51:33 -0800

    Added subscription class and arraylist to main_activity
    * subscription class with variables created
    * subscription arraylist created

--- app/src/main/java/com/example/cutiehack2020/MainActivity.kt
+++ app/src/main/java/com/example/cutiehack2020/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.cutiehack2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var subscriptionList = ArrayList<Subscription>()
+    }
+
+    class Subscription(__name: String, __price: Int, __initialStartDate: LocalDateTime, __subscriptionDuration: Int) {
+        var name = __name
+        var price = __price
+        var initialStartDate = __initialStartDate
+        var subscriptionDuration = __subscriptionDuration
     }
 }
